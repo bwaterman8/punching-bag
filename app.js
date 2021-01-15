7989b31a4e1d38f6e665bf24b2da4b6e6320d41a
Brian Waterman <bwaterman8@gmail.com>
2021-01-15 12:30:12 -0500

    all data, methods and events have been made. punch bag game is complete

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 new Vue({
     el: '#vue-app',
     data: {
-        
+        health: 100,
+        ended: false
     },
     methods: {
+        punch: function() {
+            this.health -= 5
+            if(this.health <= 0) {
+                this.ended = true
+            }
+        },
+        reset: function() {
+            this.health = 100
+            this.ended = false
+        }
         
     },
     computed: {
